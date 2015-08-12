a4724e1859168774372e9fec1100dc05ff978253
Katie Schaffer <k8e@COMPUTERTRON.local>
2015-08-12 00:20:03 -0500

    Simple example socket.io chat box with added user names and tracking.

---
+++ index.js
@@ -0,0 +1,45 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var userCount = 0;
+var users = {};
+
+io.sockets.on('connection', function(client_socket){
+  console.log('>>> * Received connection. *');
+  //console.log(client_socket);
+  client_socket.on('new guy', function(name){
+    users[client_socket.id] = {"name" : name};
+    console.log('>>> Added new user: ' + users[client_socket.id].name);
+    userCount=userCount+1;
+    console.log('>>>  ' + userCount + ' users online.');
+    console.log(users);
+    io.emit('chat message', ">>> User \"" + name + "\" has joined the chat <<<");
+  });
+  client_socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  client_socket.on('disconnect', function(socket){
+    console.log('>>> * Received disconnection. *');
+    if (users[client_socket.id]) {
+      var rmname = users[client_socket.id].name;
+      delete users[client_socket.id];
+      userCount=userCount-1;
+      console.log('>>> Removed user: ' + rmname);
+      console.log('>>>  ' + userCount + ' users online.');
+      console.log(users);
+      io.emit('chat message', ">>> User \"" + rmname + "\" has disconnected <<<");
+    }
+    else {
+      console.log(">>> False alarm. Nothing happen.");
+    }
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
