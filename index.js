1e69fcd3899481b89a40b5b6d64aad34aadbda9a
k8e <k8eSchaffer@gmail.com>
2016-03-01 21:34:36 -0600

    Cleaned up logs, added timestamps

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var timestamp;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -13,12 +14,13 @@ var userCount = 0;
 var users = {};
 
 io.sockets.on('connection', function(client_socket){
-  console.log('>>> * Received connection. *');
+  // Received a connection
   client_socket.on('new guy', function(name){
     users[client_socket.id] = {"name" : name};
-    console.log('>>> Added new user: ' + users[client_socket.id].name);
+    // New user added to chat
+    timestamp = new Date().toString();
     userCount=userCount+1;
-    console.log('>>>  ' + userCount + ' users online.');
+    console.log( timestamp + " > " + userCount + " online | " + users[client_socket.id].name + " joined.");
     io.emit('room update', users);
     io.emit('announcement', "User \"" + name + "\" has joined the chat.");
   });
@@ -26,13 +28,13 @@ io.sockets.on('connection', function(client_socket){
     io.emit('chat message', msg);
   });
   client_socket.on('disconnect', function(socket){
-    console.log('>>> * Received disconnection. *');
+    // Received a disconnection
     if (users[client_socket.id]) {
       var rmname = users[client_socket.id].name;
       delete users[client_socket.id];
       userCount=userCount-1;
-      console.log('>>> Removed user: ' + rmname);
-      console.log('>>>  ' + userCount + ' users online.');
+      timestamp = new Date().toString();
+      console.log( timestamp + " > " + userCount + " online | " + rmname + " left.");
       io.emit('room update', users);
       io.emit('announcement', "User \"" + rmname + "\" has disconnected.");
     }
@@ -43,5 +45,6 @@ io.sockets.on('connection', function(client_socket){
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  timestamp = new Date().toString();
+  console.log(timestamp + " > " + 'Now listening on *:3000');
 });
